a23e91c2d5314165b6a209a6c774f8f744421e2a
root <root@d3a2e0704417>
2017-06-02 17:47:29 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_TntkYieUWi",
+  "homePage" : "Main",
+  "name" : "Auto_TntkYieUWi",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
